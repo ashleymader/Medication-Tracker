e8a40f8ec132d9f2c03a7a05c2a01fee1a8d5009
ashleymader <ashley.mader12@gmail.com>
2020-12-11 08:17:57 -0500

    scope

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,14 +4,13 @@ class Patient < ApplicationRecord
      has_many :medications, dependent: :destroy, :through => :illnesses
      accepts_nested_attributes_for :illnesses 
        
-     def self.medication_attributes(params)
-      
-     end
+    
     #  scope :patient_quant, lambda {where(patients: :asc)}
       #  scope :med_quant, lambda {joins(:medication).(patient).(illness)where("COUNT(medications) > ?", count)} 
     #  scope :med_quant, lambda {where(medications: :asc).first}
-    scope :patient_quant, lambda {where("COUNT(patients) > ?", count)}
-    binding.pry
+
+    scope :most_illnesses, ->  {joins(:illnesses).group(:id).order('count(patients.id)desc')}
+
      #join medications table and maybe join illnesses 
      def self.search(params)
        where("LOWER(name) LIKE ?", "%#{params}%")
